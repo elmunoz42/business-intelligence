3fe82d2a237a8040fa66131006e35d93434bdd9d
Carlos Munoz Kampff <carlos@fountain-city.com>
2024-06-16 10:34:26 -0700

    added mean and median calculations to output also changed nomenclature to not be sales specific.

--- standard-dev-app.py
+++ standard-dev-app.py
@@ -1,23 +1,33 @@
 import math
 
-# Your sales numbers
-sales_input = input("Please enter your sales numbers, separated by commas: ")
+# Your values
+data_input = input("Please enter your values, separated by commas: ")
 
 # Convert the input string to a list of floats
-sales = [float(x) for x in sales_input.split(',')]
+data = [float(x) for x in data_input.split(',')]
 
-# Mean of the sales numbers
-mean = sum(sales) / len(sales)
+# Mean of the data numbers
+mean = sum(data) / len(data)
 
-# Variance of the sales numbers
-variance = sum((x - mean) ** 2 for x in sales) / len(sales)
+# Median of the data numbers
+data.sort()
+n = len(data)
+if n % 2 == 0:
+    median = (data[n // 2 - 1] + data[n // 2]) / 2
+else:
+    median = data[n // 2]
 
-# Standard deviation of the sales numbers
+# Variance of the data numbers
+variance = sum((x - mean) ** 2 for x in data) / len(data)
+
+# Standard deviation of the data numbers
 std_dev = math.sqrt(variance)
 
-# Range of sales within 68% of the mean
+# Range of data within 68% of the mean
 lower_bound = mean - std_dev
 upper_bound = mean + std_dev
-
+print(f"Mean: {mean}")
+print(f"Median: {median}")
+print(f"Variance: {variance}")
 print(f"Standard Deviation: {std_dev}")
-print(f"Range of sales within 68% of the mean: {lower_bound} to {upper_bound}")
+print(f"Range of data within 68% of the mean: {lower_bound} to {upper_bound}")
